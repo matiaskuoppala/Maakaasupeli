d7e33996aac49cb3d63a2922cb1e672e67dfec0e
matiaskuoppala <matias.kuoppala@aalto.fi>
2017-05-09 12:35:04 +0300

    Pieniä muutoksia

--- game.js
+++ game.js
@@ -36,9 +36,9 @@
         game.load.audio("gamemusic", "Laser Groove.mp3");
     }
     
-    function hitSpike() {
+    function gameOver() {
         player.kill();
-        game.add.text(player.x - 170, 143, "Game Over!", {
+        game.add.text(game.camera.x + 230, 143, "Game Over!", {
         font: "65px Arial",
         fill: "#000000",
         align: "center"
@@ -47,11 +47,11 @@
 
     var doorOpen;
     var door;
-    var pipeCollected;
+    var pipesCollected = 0;
     var pipes;
     var player;
     var dir = "right";
-    var hMove = 110;
+    var hMove = 400;
     var vMove = -300;
     var jumpTimer = 0;
     var pipesound;
@@ -68,9 +68,8 @@
 
         //  Add player sprite
         player = game.add.sprite(64, 300, "player");
-
         game.camera.follow(player);
-
+        
         //  Set up enemies
         this.enemies = game.add.physicsGroup();
         this.enemies.create(576, 64, 'enemy');
@@ -92,7 +91,7 @@
         this.pipes.create(1841, 128, 'pipe');
         this.pipes.create(2273, 352, 'pipe');
         this.pipes.create(2528, 352, 'pipe');
-        this.pipes.create(3136, 96, 'pipe');
+        this.pipes.create(3104, 96, 'pipe');
         this.pipes.create(3232, 96, 'pipe');
         this.pipes.create(3552, 352, 'pipe');
         this.pipes.forEach(function(obj) {
@@ -108,9 +107,15 @@
         game.physics.enable(this.door);
         this.door.body.immovable = true;
 
-        //  Enable physics
+        //  Enable physics for player
         game.physics.enable(player);
         
+        // Score
+        game.add.text(game.camera.x + 230, 5, "Pipes collected: " + pipesCollected, {
+        font: "30px Arial",
+        fill: "#000000",
+        align: "center"
+        });
         
         //  Set player to collide with world bounds
         player.body.collideWorldBounds = true;
@@ -125,7 +130,7 @@
         map.addTilesetImage('tileset');
         
         //  Player and spike collision
-        map.setTileIndexCallback(1, hitSpike, this);
+        map.setTileIndexCallback(1, gameOver, this);
 
         //  Create our layer
         layer = map.createLayer(0);
@@ -140,7 +145,7 @@
         //  Door collision
         map.setTileIndexCallback(9, hitDoor, this);
 
-        player.anchor.setTo(0.5, 1);
+        player.anchor.setTo(0.5, 0.5);
 
         //  Game sounds
         pipesound = game.add.audio("pipesound");
@@ -150,15 +155,21 @@
         //sounds take time to decode, this notifies when they are ready to use
         //game.sound.setDecodedCallback([ pipesound ], start, this);
 
-        mutebutton = game.add.button(2,2, "mutebuttonImage", mute, this)
+        mutebutton = game.add.button(2,2, "mutebuttonImage", mute, this);
         //mutebutton.onInputOver.add(over, this);
         mutebutton.frame = 1;
     }
 
     function update() {
-        //  Set player to collide with tiles
+        //  Player collision with tiles
         game.physics.arcade.collide(player, layer);
-
+        
+        //  Player collision with pipes
+        game.physics.arcade.collide(player, this.pipes, collectPipe, null, this);
+        
+        //  Player collision with enemies
+        game.physics.arcade.collide(player, this.enemies, hitEnemy, null, this);
+        
         //  Reset player velocity
         player.body.velocity.x = 0;
 
@@ -180,6 +191,9 @@
             player.body.velocity.y = vMove;
             jumpTimer = game.time.now + 650;
         }
+        
+        // Mutebutton
+        mutebutton.x = game.camera.x;
 
         //  Select frame to display
         if (dir == "left") {
@@ -188,12 +202,9 @@
             player.frame = 0;
         }
 
-        //  Collisions
-        game.physics.arcade.collide(this.pipe, player, collectPipe, null, this);
-
         //  Check for open door
         if (this.doorOpen) {
-            game.physics.arcade.collide(this.door, player)
+            game.physics.arcade.collide(this.door, player);
         }
         
         //console.log(player.x);
@@ -201,17 +212,38 @@
         }
 
     function collectPipe(item, item2) {
-        this.pipeCollected = true;
-        this.doorOpen = true;
+        this.pipesCollected += 1;
         pipesound.play();
-        item.destroy();
+        item2.destroy();
+        if (pipesCollected == 10) {
+            doorOpen = true;
+        }
     }
 
     function hitDoor(obj1, obj2) {
         if (doorOpen) {
-        obj1.frame = 1;
-        obj1.body.impassable = false;
+        obj2.frame = 1;
+        win();
         }
+    }
+    
+    function hitEnemy(p, e) {
+        if (p.y < e.y) {
+            e.destroy();
+            p.body.velocity.y = -200;
+            jumpTimer = game.time.now + 650;
+        } else {
+            gameOver();
+        }
+    }
+    
+    function win() {
+        player.kill();
+        game.add.text(game.camera.x + 280 , 155, "You Win!", {
+        font: "65px Arial",
+        fill: "#000000",
+        align: "center"
+    });
     }
 
     function mute() {
